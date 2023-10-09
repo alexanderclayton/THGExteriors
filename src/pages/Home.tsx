c69c9cb2347ac9135ab7088c672b496a68201599
Alex Clayton <alexander.clayton.email@gmail.com>
2023-10-09 00:39:08 -0500

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 //import//
+import Hero from "../assets/THG Large.png";
 
 export const Home = () => {
   return (
-    <div className="font-bold">Hello World!</div>
-  )
-}
+    <div className="flex items-center h-screen">
+      <div className="w-[50%] flex justify-end">
+        <img src={Hero} alt="thg logo" className="h-96" />
+      </div>
+      <div className="w-[30%]">
+        <p className="font-bold text-9xl">THG<span className="text-8xl text-red-600">Exteriors</span></p>
+        <p className="text-2xl flex justify-around"><span>Quality </span><span>Custom </span><span>Exteriors</span></p>
+      </div>
+    </div>
+  );
+};
