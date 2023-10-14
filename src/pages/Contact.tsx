bdde947dfdd1a5a3113cc1470edfd6bf6dd9613b
Alex Clayton <alexander.clayton.email@gmail.com>
2023-10-14 18:33:45 -0500

    basic styles emailjs form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -16,15 +16,15 @@ export const Contact = () => {
       });
   }
   return (
-    <div>
-      <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" className='border'/>
-      <label>Email</label>
-      <input type="email" name="user_email" className='border'/>
-      <label>Message</label>
+    <div className='flex justify-center'>
+      <form ref={form} onSubmit={sendEmail} className='flex flex-col w-[60%]'>
+      <label className='font-bold'>Name:</label>
+      <input type="text" name="user_name" className='border w-[50%]'/>
+      <label className='font-bold'>Email:</label>
+      <input type="email" name="user_email" className='border w-[50%]'/>
+      <label className='font-bold'>Message:</label>
       <textarea name="message" className='border'/>
-      <input type="submit" value="Send" />
+      <input type="submit" value="Send" className='bg-green-300 w-[25%] mt-4 py-3 border-slate-300 border rounded-lg hover:cursor-pointer'/>
     </form>
     </div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,14 @@ import Hero from "../assets/THG Large.png";
 
 export const Home = () => {
   return (
-    <div className="flex items-center h-screen">
-      <div className="w-[50%] flex justify-end">
+    <div className="flex flex-col justify-center md:flex-row md:items-center h-screen">
+      <div className="md:w-[50%] w-full flex justify-end">
         <img src={Hero} alt="thg logo" className="h-96" />
       </div>
-      <div className="w-[30%]">
-        <p className="font-bold text-9xl">THG<span className="text-8xl text-red-600">Exteriors</span></p>
-        <p className="text-2xl flex justify-around"><span>Quality </span><span>Custom </span><span>Exteriors</span></p>
+      <div className="md:w-[30%] w-full flex flex-col items-center md:items-start">
+        <p className="font-bold text-9xl">THG<span className="text-8xl text-red-600 hidden md:block">Exteriors</span></p>
+        <p className="text-7xl text-red-600">Exteriors</p>
+        <p className="w-full text-2xl flex justify-around"><span>Quality </span><span>Custom </span><span>Exteriors</span></p>
       </div>
     </div>
   );
