8149d6a17d79b00fbd50d5928eadefd8e6732cb1
Alex <alexander.clayton.email@gmail.com>
2024-01-01 14:39:42 -0600

    added clerk authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,22 @@ import Hero from "../assets/THG Large.png";
 
 export const Home = () => {
   return (
-    <div className="flex flex-col justify-center md:flex-row md:items-center h-screen">
-      <div className="md:w-[50%] w-full flex justify-end">
+    <div className="flex h-screen flex-col justify-center md:flex-row md:items-center">
+      <div className="flex w-full justify-end md:w-[50%]">
         <img src={Hero} alt="thg logo" className="h-96" />
       </div>
-      <div className="md:w-[30%] w-full flex flex-col items-center md:items-start">
-        <p className="font-bold text-9xl">THG<span className="text-8xl text-red-600 hidden md:block">Exteriors</span></p>
-        <p className="text-7xl text-red-600">Exteriors</p>
-        <p className="w-full text-2xl flex justify-around"><span>Quality </span><span>Custom </span><span>Exteriors</span></p>
+      <div className="flex w-full flex-col items-center md:w-[30%] md:items-start">
+        <p className="text-9xl font-bold">
+          THG
+          <span className="hidden text-8xl text-red-600 md:block">
+            Exteriors
+          </span>
+        </p>
+        <p className="flex w-full justify-around text-2xl">
+          <span>Quality </span>
+          <span>Custom </span>
+          <span>Exteriors</span>
+        </p>
       </div>
     </div>
   );
