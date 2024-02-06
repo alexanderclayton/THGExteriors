216ba808f0b7a44fc33a4084c3a08d974d9a59b4
Alex <alexander.clayton.email@gmail.com>
2024-02-06 08:43:20 -0600

    destructured Client.tsx into separate components:

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { TProject } from "../types";
+
+interface IProjectCardProps<T> {
+  model: T;
+}
+
+export const ProjectCard = ({ model }: IProjectCardProps<TProject[]>) => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex overflow-x-auto">
+      {model.map((project) => (
+        <div
+          key={project.id}
+          className="mb-4 mr-4 w-72 flex-shrink-0 transform cursor-pointer rounded-md border border-gray-300 p-4 transition duration-300 ease-in-out hover:scale-105"
+          onClick={() => navigate(`/project/${project.id}`)}
+        >
+          <p className="mb-2 truncate text-lg font-semibold text-gray-800">
+            {project.projectName}
+          </p>
+          <p className="mb-1 text-gray-600">
+            <span className="font-semibold">Start Date:</span>{" "}
+            {new Date(project.projectStartDate).toLocaleDateString()}
+          </p>
+          <p className="mb-1 text-gray-600">
+            <span className="font-semibold">End Date:</span>{" "}
+            {new Date(project.projectEndDate).toLocaleDateString()}
+          </p>
+          <p className="text-gray-600">
+            <span className="font-semibold">Paid:</span>{" "}
+            {project.projectPaid ? "Yes" : "No"}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/pages/Client.tsx
+++ src/pages/Client.tsx
@@ -1,6 +1,6 @@
 //import//
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import {
   TClient,
   TProject,
@@ -15,17 +15,15 @@ import {
   mapClientDocument,
   mapProjectDocument,
 } from "../services";
-import { UpdateClient } from "../components/UpdateClient";
-import { ProjectForm } from "../components/ProjectForm";
 import { RadarAddress } from "radar-sdk-js/dist/types";
 import { Map } from "../components/Map";
 import { getMap } from "../radar";
-import { FaRegImage } from "react-icons/fa6";
 import { Notes } from "../components/Notes";
+import { ClientInfo } from "../components/ClientInfo";
+import { ClientProjects } from "../components/ClientProjects";
 
 export const Client = () => {
   const params = useParams();
-  const navigate = useNavigate();
   const [client, setClient] = useState<TClient>({
     clientFirstName: "",
     clientLastName: "",
@@ -66,40 +64,7 @@ export const Client = () => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-gray-100">
       <div className="mb-4 flex w-full justify-between rounded-lg bg-white px-8 py-4 shadow-md">
-        <div className="flex flex-col justify-center">
-          <div className="mb-2 flex items-center">
-            <p className="mr-4 text-4xl font-bold text-gray-800">
-              {client.clientFirstName} {client.clientLastName}
-            </p>
-            {client.clientLastName !== "" && (
-              <UpdateClient
-                params={params}
-                model={client}
-                setFunction={setClient}
-              />
-            )}
-          </div>
-          <div className="flex">
-            <div className="mr-4">
-              <p className="text-gray-600">
-                {client.clientAddress.addressLabel}
-              </p>
-              <p className="text-gray-600">{client.clientPhone}</p>
-              <p className="text-gray-600">{client.clientEmail}</p>
-            </div>
-            <div>
-              {client.imageUrl === "" ? (
-                <FaRegImage size={60} className="text-gray-400" />
-              ) : (
-                <img
-                  src={client.imageUrl}
-                  alt="client home exterior"
-                  className="h-32 w-32 rounded-md object-cover shadow-md"
-                />
-              )}
-            </div>
-          </div>
-        </div>
+        <ClientInfo model={client} setModel={setClient} params={params} />
         <div className="">
           {client.clientAddress.latitude && (
             <Map radarFunction={getMap} model={client} />
@@ -107,55 +72,15 @@ export const Client = () => {
         </div>
       </div>
       <div className="flex w-full px-8">
-        <div className="mr-4 flex w-1/2 flex-col">
-          <div className="mb-4">
-            <p className="mb-2 text-2xl font-semibold text-gray-800">
-              Projects:
-            </p>
-            {!toggleAdd && (
-              <button
-                className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-                onClick={() => setToggleAdd(!toggleAdd)}
-              >
-                Add Project
-              </button>
-            )}
-            {toggleAdd && (
-              <ProjectForm
-                legend="Add Project"
-                model={project}
-                setState={setProject}
-                setAllState={setClientProjects}
-                submit="submit form"
-                toggle={toggleAdd}
-                setToggle={setToggleAdd}
-                params={params}
-              />
-            )}
-          </div>
-          <div className="flex">
-            {clientProjects.map((project) => (
-              <div
-                key={project.id}
-                className="mb-4 cursor-pointer rounded-md border border-gray-300 p-4 hover:bg-gray-50"
-                onClick={() => navigate(`/project/${project.id}`)}
-              >
-                <p className="text-lg font-semibold text-gray-800">
-                  {project.projectName}
-                </p>
-                <p className="text-gray-600">
-                  Start Date: {project.projectStartDate.toDateString()}
-                </p>
-                <p className="text-gray-600">
-                  End Date: {project.projectEndDate.toDateString()}
-                </p>
-                <p className="text-gray-600">
-                  Paid: {project.projectPaid ? "Yes" : "No"}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
+        <ClientProjects
+          model={project}
+          setModel={setProject}
+          setAllModel={setClientProjects}
+          cardModel={clientProjects}
+          params={params}
+          toggle={toggleAdd}
+          setToggle={setToggleAdd}
+        />
         <div className="ml-4 flex w-1/2 flex-col">
           <p className="mb-2 text-2xl font-semibold text-gray-800">Notes:</p>
           <Notes
@@ -167,22 +92,6 @@ export const Client = () => {
           />
         </div>
       </div>
-
-      {/*
-      DELETE CLIENT DOCUMENT.  ADD SECOND LAYER OF SECURITY
-      <button
-        onClick={() =>
-          deleteDocument(
-            "clients",
-            params,
-            deleteClientFields,
-            navigate,
-            "/allclients",
-          )
-        }
-      >
-        Delete Client
-      </button>*/}
     </div>
   );
 };
